acf72e45cf6b19691868416daa96b7670b45323e
stojko-debug <61667450+stojko-debug@users.noreply.github.com>
2020-12-13 03:46:37 +0100

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,20 @@
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
